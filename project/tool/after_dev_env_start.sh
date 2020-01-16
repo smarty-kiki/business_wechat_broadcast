ac9979119bfabef9b3b2129ad93a60f4f391ea1f
yaoyang <kiki@smarty.so>
2020-01-16 13:56:56 +0800

    调整supervisor配置名防止多项目混合部署队列worker重复,调整脚本位置

--- project/tool/after_dev_env_start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ENV=development php /var/www/layui_mvc_frame/public/cli.php migrate:install
-ENV=development php /var/www/layui_mvc_frame/public/cli.php migrate
---
+++ project/tool/production/after_push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+ROOT_DIR="$(cd "$(dirname $0)" && pwd)"/../../..
+
+ln -fs $ROOT_DIR/project/config/production/nginx/layui_mvc_frame.conf /etc/nginx/sites-enabled/layui_mvc_frame
+/usr/sbin/service nginx reload
+
+/bin/bash $ROOT_DIR/project/tool/dep_build.sh link
+/usr/bin/php $ROOT_DIR/public/cli.php migrate:install
+/usr/bin/php $ROOT_DIR/public/cli.php migrate
+
+ln -fs $ROOT_DIR/project/config/production/supervisor/layui_mvc_frame_queue_worker.conf /etc/supervisor/conf.d/layui_mvc_frame_queue_worker.conf
+/usr/bin/supervisorctl update
+/usr/bin/supervisorctl restart mvc_frame_queue_worker:*
+
+ln -fs $ROOT_DIR/project/config/production/crontab/layui_mvc_frame /etc/cron.d/layui_mvc_frame
