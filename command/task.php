40f3efd9a605e1dcf0edd51adbf9f4c8b21f5be0
yaoyang <kiki@smarty.so>
2020-08-11 11:06:11 +0800

    debug

--- command/task.php
+++ command/task.php
@@ -6,7 +6,7 @@ command('task:trigger', '任务触发', function ()
 
     foreach ($tasks as $task) {
         if (spider_cron_string_parse($task->crontab_rule) === time()) {
-            business_wechat_send_text_to_group_robot($task->robot_url, $tast->message);
+            business_wechat_send_text_to_group_robot($task->robot_url, $task->message);
         }
     }
 });
