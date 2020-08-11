e29b74e995cab4c386b871ac06d6e7cec3bd82a7
yaoyang <kiki@smarty.so>
2020-08-11 10:51:47 +0800

    取消队列

--- project/tool/production/after_push.sh
+++ project/tool/production/after_push.sh
@@ -9,9 +9,9 @@ ln -fs $ROOT_DIR/project/config/production/nginx/business_wechat_broadcast.conf 
 /usr/bin/php $ROOT_DIR/public/cli.php migrate:install
 /usr/bin/php $ROOT_DIR/public/cli.php migrate
 
-ln -fs $ROOT_DIR/project/config/production/supervisor/business_wechat_broadcast_queue_worker.conf /etc/supervisor/conf.d/business_wechat_broadcast_queue_worker.conf
-/usr/bin/supervisorctl update
-/usr/bin/supervisorctl restart business_wechat_broadcast_queue_worker:*
+#ln -fs $ROOT_DIR/project/config/production/supervisor/business_wechat_broadcast_queue_worker.conf /etc/supervisor/conf.d/business_wechat_broadcast_queue_worker.conf
+#/usr/bin/supervisorctl update
+#/usr/bin/supervisorctl restart business_wechat_broadcast_queue_worker:*
 
 chmod 777 /var/www/business_wechat_broadcast/view/blade
 rm -rf /var/www/business_wechat_broadcast/view/blade/*.php
