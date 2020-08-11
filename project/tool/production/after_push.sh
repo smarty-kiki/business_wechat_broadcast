ce7eb6c4eaa1c78a44bdf7cbc9666c73ccb28fdc
yaoyang <kiki@smarty.so>
2020-08-11 10:19:43 +0800

    Fork from git@github.com:smarty-kiki/layui_mvc_frame.git and modify project name

--- project/tool/production/after_push.sh
+++ project/tool/production/after_push.sh
@@ -2,16 +2,16 @@
 
 ROOT_DIR="$(cd "$(dirname $0)" && pwd)"/../../..
 
-ln -fs $ROOT_DIR/project/config/production/nginx/layui_mvc_frame.conf /etc/nginx/sites-enabled/layui_mvc_frame
+ln -fs $ROOT_DIR/project/config/production/nginx/business_wechat_broadcast.conf /etc/nginx/sites-enabled/business_wechat_broadcast
 /usr/sbin/service nginx reload
 
 /bin/bash $ROOT_DIR/project/tool/dep_build.sh link
 /usr/bin/php $ROOT_DIR/public/cli.php migrate:install
 /usr/bin/php $ROOT_DIR/public/cli.php migrate
 
-ln -fs $ROOT_DIR/project/config/production/supervisor/layui_mvc_frame_queue_worker.conf /etc/supervisor/conf.d/layui_mvc_frame_queue_worker.conf
+ln -fs $ROOT_DIR/project/config/production/supervisor/business_wechat_broadcast_queue_worker.conf /etc/supervisor/conf.d/business_wechat_broadcast_queue_worker.conf
 /usr/bin/supervisorctl update
-/usr/bin/supervisorctl restart layui_mvc_frame_queue_worker:*
+/usr/bin/supervisorctl restart business_wechat_broadcast_queue_worker:*
 
-chmod 777 /var/www/layui_mvc_frame/view/blade
-rm -rf /var/www/layui_mvc_frame/view/blade/*.php
+chmod 777 /var/www/business_wechat_broadcast/view/blade
+rm -rf /var/www/business_wechat_broadcast/view/blade/*.php
--- project/tool/start_development_server.sh
+++ project/tool/start_development_server.sh
@@ -4,13 +4,13 @@ ROOT_DIR="$(cd "$(dirname $0)" && pwd)"/../..
 
 sh $ROOT_DIR/project/tool/dep_build.sh link
 
-sudo docker run --rm -ti -p 80:80 -p 8080:8080 -p 3306:3306 --name layui_mvc_frame \
+sudo docker run --rm -ti -p 80:80 -p 8080:8080 -p 3306:3306 --name business_wechat_broadcast \
     -v $ROOT_DIR/../frame:/var/www/frame \
-    -v $ROOT_DIR/:/var/www/layui_mvc_frame \
-    -v $ROOT_DIR/project/config/development/nginx/layui_mvc_frame.conf:/etc/nginx/sites-enabled/default \
-    -v $ROOT_DIR/project/config/development/supervisor/layui_mvc_frame_queue_worker.conf:/etc/supervisor/conf.d/queue_worker.conf \
-    -e 'PRJ_HOME=/var/www/layui_mvc_frame' \
+    -v $ROOT_DIR/:/var/www/business_wechat_broadcast \
+    -v $ROOT_DIR/project/config/development/nginx/business_wechat_broadcast.conf:/etc/nginx/sites-enabled/default \
+    -v $ROOT_DIR/project/config/development/supervisor/business_wechat_broadcast_queue_worker.conf:/etc/supervisor/conf.d/queue_worker.conf \
+    -e 'PRJ_HOME=/var/www/business_wechat_broadcast' \
     -e 'ENV=development' \
     -e 'TIMEZONE=Asia/Shanghai' \
-    -e 'AFTER_START_SHELL=/var/www/layui_mvc_frame/project/tool/development/after_env_start.sh' \
+    -e 'AFTER_START_SHELL=/var/www/business_wechat_broadcast/project/tool/development/after_env_start.sh' \
 kikiyao/debian_php_dev_env start
