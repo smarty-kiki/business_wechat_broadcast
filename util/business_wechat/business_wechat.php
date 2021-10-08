a964b6cc351dbc2dacdc061d5fa9e7fe72ce345b
yaoyang <kiki@smarty.so>
2021-10-08 22:03:07 +0800

    更换框架从 remote 方法改为 http 方法

--- util/business_wechat/business_wechat.php
+++ util/business_wechat/business_wechat.php
@@ -2,12 +2,15 @@
 
 function business_wechat_send_text_to_group_robot($url, $message, $mentioned_list = [], $mentioned_mobile_list = [])
 {
-    return remote_post($url, json([
-        'msgtype' => 'text',
-        'text' => [
-            'content' =>  $message,
-            "mentioned_list" => $mentioned_list,
-            "mentioned_mobile_list" => $mentioned_mobile_list,
-        ],
-    ]));
+    return http([
+        'url' => $url,
+        'data' => json([
+            'msgtype' => 'text',
+            'text' => [
+                'content' =>  $message,
+                "mentioned_list" => $mentioned_list,
+                "mentioned_mobile_list" => $mentioned_mobile_list,
+            ],
+        ]),
+    ]);
 }
